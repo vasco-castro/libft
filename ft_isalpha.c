bdf321e5b20747193dc5e6a4dadf70583fdf16ca
Vasco Soares Sande E Castro <vsoares-@c2r1s7.42lisboa.com>
2024-10-28 16:16:34 +0000

    HEader file plus IS functions

---
+++ ft_isalpha.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsoares- <vsoares-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 14:13:26 by vsoares-          #+#    #+#             */
+/*   Updated: 2024/10/28 15:47:12 by vsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
+
+static int	ft_islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+int	ft_isalpha(int c)
+{
+	return (ft_isupper(c) || ft_islower(c));
+}
---
+++ libft.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsoares- <vsoares-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 15:31:02 by vsoares-          #+#    #+#             */
+/*   Updated: 2024/10/28 15:55:03 by vsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+/* IS FUNCTIONS */
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(char c);
+
+// bzero
+// memcpy
+// memmove
+
+// strlcpy
+// strlcat
+
+// toupper
+// tolower
+
+// strchr
+// strrchr
+// strncmp
+
+// memchr
+// memcmp
+
+// strnstr
+// atoi
+
+#endif
