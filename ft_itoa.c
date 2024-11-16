d72384c5ecf1927ebc7e3cffc490cfe33c6f3cde
Vasco Soares Sande E Castro <vsoares-@c2r3s6.42lisboa.com>
2024-11-16 21:12:02 +0000

    itoa TESTED

---
+++ ft_itoa.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsoares- <vsoares-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 19:52:45 by vsoares-          #+#    #+#             */
+/*   Updated: 2024/11/16 21:10:43 by vsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	nbrsize(long nb)
+{
+	size_t	count;
+
+	count = 0;
+	if (nb == 0)
+		return (1);
+	if (nb < 0)
+	{
+		nb *= -1;
+		count++;
+	}
+	while(nb > 0)
+	{
+		nb /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*number;
+	long	nb;
+	size_t	nlen;
+
+	nb = n;
+	nlen = nbrsize(nb);
+	number = ft_calloc(nlen + 1, 1);
+	if (!number)
+		return (NULL);
+	if (nb == 0)
+		number[0] = '0';
+	if (nb < 0)
+	{
+		number[0] = '-';
+		nb *= -1;
+	}
+	while (nb > 0)
+	{
+		number[--nlen] = nb % 10 + '0';
+		nb /= 10;
+	}
+	return (number);
+}
